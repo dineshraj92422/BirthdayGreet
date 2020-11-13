7542c86a868800f2b9b766d99e12c85afbb94820
dineshraj92422 <dineshraj9724226506@gmail.com>
2020-11-14 03:10:32 +0530

    completed basic project

---
+++ app/src/main/java/com/dmak/birthdaygreeting/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.dmak.birthdaygreeting
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btncreate.setOnClickListener {
+            createBirthdayCard()
+        }
+    }
+
+     fun createBirthdayCard() {
+         val inputName = name.editableText.toString()
+
+         val intent = Intent(this,BdayGreetingActivity::class.java)
+         intent.putExtra(BdayGreetingActivity.NAME_EXTRA,inputName)
+         startActivity(intent)
+
+    }
+
+
+}
